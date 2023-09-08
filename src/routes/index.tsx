aace6fbccd36caab383e8eb8c194dc24a534532b
mahi1212 <mahi121.mr@gmail.com>
2023-09-08 21:07:54 +0600

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../page/Home";
+import Login from "../page/Login";
+import App from "../App";
+import NotFound from "../page/NotFound";
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    },
+    {
+        path: "/home",
+        element: <Home />
+    },
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "*",
+        element: <NotFound />
+    },
+
+]);
+
+
+export default routes;
